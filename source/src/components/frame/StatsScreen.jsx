8a7673f4fb6590356d71bcb60693ced1e09f24fc
Josh-Green <joshgreen95@protonmail.com>
2022-12-30 02:42:58 +0000

    Added stats to stats page
    Stats page still needs styling.

--- source/src/components/frame/StatsScreen.jsx
+++ source/src/components/frame/StatsScreen.jsx
@@ -1,10 +1,52 @@
 //Core
 import React from 'react';
+import { GetStats } from '../../modules/gamelogic/Stats';
+import StatGraph from '../stats/StatGraph';
 
 export default function StatsScreen() {
+    let wins = 0;
+    let losses = 0;
+    let gamesPlayed = 0;
+    let winPercent = 0;
+    const stats = GetStats();
+    console.log(stats);
+
+    if(stats != null) {
+        gamesPlayed = stats.length;
+        
+        stats.forEach((value) => {
+            if(value >= 5) { losses ++ ; }
+            else { wins ++ ; } 
+            console.log(`wins ${wins} losses ${losses}`);
+        }); 
+    
+        winPercent = Math.floor((losses/wins) * 100);
+    };
+
+
+
     return (
+        <>
             <div className='overlaywindow'> 
-                This is the Stats Screen
+                <div className='statboxcontainer'>
+                    <div id='statbox'>
+                        <h3>{gamesPlayed}</h3>
+                        <h4>Played</h4>
+                    </div>
+                    <div id='statbox'>
+                        <h3>{winPercent}</h3>
+                        <h4>Win %</h4>
+                    </div>
+                </div>
+                {stats &&(<StatGraph />)}
+                <input type={'button'} onClick={() => ClearStats()} />
             </div>
+        </>
+
   )
+}
+
+function ClearStats(){
+    alert('Your Stats have been cleared');
+    localStorage.removeItem('stats');
 }
--- source/src/components/stats/StatGraph.jsx
+++ source/src/components/stats/StatGraph.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-import { GetStats } from '../../modules/gamelogic/Stats'
+//core
+import React from 'react';
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from 'recharts';
+//modules
+import { GetStats } from '../../modules/gamelogic/Stats';
 
 export default function StatGraph() {
-    let stats = GetStats();
+  
+  const stats = GetStats();
+  const statsObj = [
+    {
+      name: '1',
+      num: 0
+    },
+    {
+      name: '2',
+      num: 0
+    },
+    {
+      name: '3',
+      num: 0
+    },
+    {
+      name: '4',
+      num: 0
+    },
+    {
+      name: 'fails',
+      num: 0
+    }]
 
-    return React.createElement(
+
+
+
+  stats.forEach((value) => { 
+    console.log(statsObj[value - 1]);
+    if(value >= 5) {
+      statsObj[4]['num'] += 1;
+    } else {
+      statsObj[value -1]['num'] += 1;
+    }
+  });
+
+
+  return (
+    <>
+    <h5>Guess Distribution</h5>
+    <BarChart  width={400} height={200} data={statsObj} layout={'vertical'} label='Guess Distribution' margin={{top: 0}}>
+      <XAxis dataKey={'num'} type={'number'} axisLine={false} tick={false}/>
+      <YAxis dataKey={'name'} minTickGap={0} tickMargin={2} type={'category'} tickLine={false} tick={'font-family: inherit'}/>
+      <Bar dataKey={'num'} scale={0.2} fill={'#3a3a3c'} label={'num'} minPointSize={20} >
+        <LabelList dataKey={'num'} position={'end'} fill={'white'}/>
+      </Bar>
+    </BarChart>
+    </>
   )
 }
