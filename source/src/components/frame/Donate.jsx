498002cd249e551230ab2e1520b99219663a38bd
Josh-Green <joshgreen95@protonmail.com>
2023-01-02 22:33:18 +0000

    Fixed Typo
    Fixed typo in donate.

--- source/src/components/frame/Donate.jsx
+++ source/src/components/frame/Donate.jsx
@@ -6,7 +6,7 @@ export default function Donate() {
     <div className='donate'>
           <h5>Wowdle doesn't use ads, consider showing some support by <a href='https://www.buymeacoffee.com/joshgreen' target='_blank'>Donating.</a></h5>
           <h5>Check out my <a href='https://github.com/joshgreen95' target='_blank'>GitHub.</a></h5>    
-          <h5><a href='https://joshgreen.tech' target='_blank'><div id='begging'>My Personal Site. Im desperate for a job... please please hire me.</div></a></h5>
+          <h5><a href='https://joshgreen.tech' target='_blank'><div id='begging'>My Personal Site. I'm Open for work.</div></a></h5>
     </div>
   )
 }
