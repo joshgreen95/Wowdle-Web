e52fe2b59d4bf1d647a66f92b4554ee2eb31212d
Josh-Green <joshgreen95@protonmail.com>
2023-09-18 01:21:07 +0100

    Update StatGraph.jsx

--- source/src/components/stats/StatGraph.jsx
+++ source/src/components/stats/StatGraph.jsx
@@ -5,42 +5,46 @@ import { Bar, BarChart, CartesianGrid, LabelList, ResponsiveContainer, XAxis, YA
 import { GetStats } from '../../modules/gamelogic/Stats';
 
 export default function StatGraph() {
-  
   const stats = GetStats();
   const statsObj = [
     {
       name: '1',
-      num: 0
+      'num': 0
     },
     {
       name: '2',
-      num: 0
+      'num': 0
     },
     {
       name: '3',
-      num: 0
+      'num': 0
     },
     {
       name: '4',
-      num: 0
+      'num': 0
     },
     {
       name: 'fails',
-      num: 0
+      'num': 0
     }]
 
-  if(stats && stats.isArray){
-    stats.forEach((value) => { 
-    console.log(statsObj[value - 1]);
-    if(value >= 5) {
-      statsObj[4]['num'] += 1;
-    } else {
-      statsObj[value - 1]['num'] += 1;
+    console.log(stats, stats.length > 1);
+    if(stats && stats.length > 1){
+      stats.forEach((value) => { 
+        if(value !== ''){
+          if (value >= 5) {
+            statsObj[4]['num'] += 1;
+          } else {
+            statsObj[value - 1]['num'] += 1;
+          }
+        }
+        });  
+    } else if(stats.length === 1){
+      statsObj[stats - 1]['num'] += 1;
     }
-  });  
-} else if(stats){
-  statsObj[stats - 1]['num'] += 1;
-}
+
+    console.log(statsObj);
+
   return (
     <>
     <h5>Guess Distribution</h5>
